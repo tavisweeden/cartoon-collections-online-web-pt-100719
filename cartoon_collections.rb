a7b0e30bc35c34de46c6b35c131e09ad1f7ff23e
Tavis Weeden <tavisweeden@gmail.com>
2019-10-20 19:27:26 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -21,7 +21,7 @@ end
 def find_the_cheese(snacks)
   cheese_types = ["cheddar", "gouda", "camembert"]
   #if snacks.include?(cheese_types)
-  snacks.find{|elem| elem == snacks}  
+  snacks.find{|elem| elem == cheese_types}  
     
 
 
